fbe604f80cf7ea2904faf9727558100ac08d0754
ecmedeiros <elias.demedeiros@hotmail.com>
2022-08-05 17:30:20 -0300

    links e nome trocam conforme a cidade

--- js/main.js
+++ js/main.js
@@ -1,28 +1,43 @@
-function refreshSelect(){
-    let select = document.querySelector('#cidades');
-    const optionValue = select.options[select.selectedIndex]; //trata o selected como um array
+function refreshSelect() {
+  let select = document.querySelector("#cidades");
+  const optionValue = select.options[select.selectedIndex]; //trata o selected como um array
+  let value = optionValue.value;
 
-    let value = optionValue.value;
-    const cafeterias = document.querySelector('.container');
-    h2 = document.querySelector('.nome-cafe');
+  const cafe = document.querySelectorAll(".nome-cafe");
+  const site = document.querySelectorAll(".site");
+  //const instagram = document.querySelectorAll('.nome-cafe');
 
-    if (value === 'campo-bom'){
-        h2.innerHTML = `Cafeteria de Campo bom`;
-    }else if(value === 'novo-hamburgo') {
-        h2.innerHTML = `Cafeteria de novo hamburgo`;
-    }
+  const nomescafe = [
+    "Quiero Café",
+    "Me Gusta cafeteria",
+    "D'cakes Boleria E Café",
+    "La Fourmi Crêperie et Café",
+    "Grão Brasil Café",
+    "Artesana cafeteria",
+  ];
 
+  const sites = [
+    "https://quierocafe.com.br/",
+    "https://www.instagram.com/megusta.cafeteria/",
+    "https://linklist.bio/dcakesboleria",
+  ];
+
+  //for (var i = 0; i < cafe.length; i++) {
+  if (value === "campo-bom") {
+    cafe[0].innerHTML = nomescafe[0];
+    cafe[1].innerHTML = nomescafe[1];
+    cafe[2].innerHTML = nomescafe[2];
+
+    site[0].href = sites[0];
+    site[1].href = sites[1];
+    site[2].href = sites[2];
+    
+  } else if (value === "novo-hamburgo") {
+    cafe[0].innerHTML = nomescafe[3];
+    cafe[1].innerHTML = nomescafe[4];
+    cafe[2].innerHTML = nomescafe[5];
+  }
 }
-
-
-
-cafeteria1 = {
-    nome: 'cafundo de judas',
-    site: 'www.fimdomundo.com',
-    instagram: 'instagram/judas.com.br'
-}
-
-
-
+//}
 
 refreshSelect();
