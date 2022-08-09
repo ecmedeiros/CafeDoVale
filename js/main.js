ebefe7df7b611bea3eefec350a469eca24affa63
ecmedeiros <elias.demedeiros@hotmail.com>
2022-08-09 16:53:39 -0300

    links atualizados

--- js/main.js
+++ js/main.js
@@ -20,12 +20,18 @@ function refreshSelect() {
     "https://quierocafe.com.br/",
     "https://linktr.ee/chafariz",
     "https://linklist.bio/dcakesboleria",
+    "https://linktr.ee/lafourmicreperie",
+    "https://cafegraobrasil.com.br/index.html",
+    "http://www.artesanachocolates.com.br/"
   ];
 
   const instaLink = [
     "https://www.instagram.com/quierocafeoficial/",
     "https://www.instagram.com/postoschafariz/",
     "https://www.instagram.com/dcakesboleriaecafe/",
+    "https://www.instagram.com/lafourmicreperie/",
+    "https://www.instagram.com/graobrasilcafe/",
+    "https://www.instagram.com/artesana.cafeteria/"
   ];
 
   //for (var i = 0; i < cafe.length; i++) {
@@ -55,6 +61,14 @@ function refreshSelect() {
     cafe[0].innerHTML = nomescafe[3];
     cafe[1].innerHTML = nomescafe[4];
     cafe[2].innerHTML = nomescafe[5];
+
+    site[0].href = sitesLinks[3];
+    site[1].href = sitesLinks[4];
+    site[2].href = sitesLinks[5];
+
+    instagram[0].href = instaLink[3];
+    instagram[1].href = instaLink[4];
+    instagram[2].href = instaLink[5];
   } else if (value === "select") {
     for (var i = 0; i < cafe.length; i++) {
       cafe[i].innerHTML = "";
