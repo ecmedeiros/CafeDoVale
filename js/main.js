c17e5bbbf391896ad6276f9b88dfa5ea28b26d9f
ecmedeiros <elias.demedeiros@hotmail.com>
2022-08-24 12:48:42 -0300

    javascript otimizado

--- js/main.js
+++ js/main.js
@@ -7,79 +7,100 @@ function refreshSelect() {
   const site = document.querySelectorAll(".site");
   const instagram = document.querySelectorAll(".instagram");
   const links = document.querySelectorAll(".links");
-  const caixaCafe = document.querySelectorAll('.box-cafe')
+  const caixaCafe = document.querySelectorAll(".box-cafe");
 
-  const nomescafe = [
-    "Quiero Café",
-    "Chafariz",
-    "D'cakes Boleria E Café",
-    "La Fourmi Crêperie et Café",
-    "Grão Brasil Café",
-    "Artesana cafeteria",
+  const cafeteriasCb = [
+    {
+      nome: "Quiero Café",
+      siteUrl: "https://quierocafe.com.br/",
+      insta: "https://www.instagram.com/quierocafeoficial/",
+    },
+    {
+      nome: "Chafariz",
+      siteUrl: "https://linktr.ee/chafariz",
+      insta: "https://www.instagram.com/postoschafariz/",
+    },
+    {
+      nome: "D'cakes Boleria E Café",
+      siteUrl: "https://linklist.bio/dcakesboleria",
+      insta: "https://www.instagram.com/dcakesboleriaecafe/",
+    },
   ];
 
-  const sitesLinks = [
-    "https://quierocafe.com.br/",
-    "https://linktr.ee/chafariz",
-    "https://linklist.bio/dcakesboleria",
-    "https://linktr.ee/lafourmicreperie",
-    "https://cafegraobrasil.com.br/index.html",
-    "http://www.artesanachocolates.com.br/",
+  const cafeteriasNh = [
+    {
+      nome: "La Fourmi Crêperie et Café",
+      siteUrl: "https://linktr.ee/lafourmicreperie",
+      insta: "https://www.instagram.com/lafourmicreperie/",
+    },
+    {
+      nome: "Grão Brasil Café",
+      siteUrl: "https://cafegraobrasil.com.br/index.html",
+      insta: "https://www.instagram.com/graobrasilcafe/",
+    },
+    {
+      nome: "Artesana cafeteria",
+      siteUrl: "http://www.artesanachocolates.com.br/",
+      insta: "https://www.instagram.com/artesana.cafeteria/",
+    },
   ];
 
-  const instaLink = [
-    "https://www.instagram.com/quierocafeoficial/",
-    "https://www.instagram.com/postoschafariz/",
-    "https://www.instagram.com/dcakesboleriaecafe/",
-    "https://www.instagram.com/lafourmicreperie/",
-    "https://www.instagram.com/graobrasilcafe/",
-    "https://www.instagram.com/artesana.cafeteria/",
-  ];
+  //#region Codigo antigo
+  // const nomescafe = [
+  //   "Quiero Café",
+  //   "Chafariz",
+  //   "D'cakes Boleria E Café",
+  //   "La Fourmi Crêperie et Café",
+  //   "Grão Brasil Café",
+  //   "Artesana cafeteria",
+  // ];
 
+  // const sitesLinks = [
+  //   "https://quierocafe.com.br/",
+  //   "https://linktr.ee/chafariz",
+  //   "https://linklist.bio/dcakesboleria",
+  //   "https://linktr.ee/lafourmicreperie",
+  //   "https://cafegraobrasil.com.br/index.html",
+  //   "http://www.artesanachocolates.com.br/",
+  // ];
 
+  // const instaLink = [
+  //   "https://www.instagram.com/quierocafeoficial/",
+  //   "https://www.instagram.com/postoschafariz/",
+  //   "https://www.instagram.com/dcakesboleriaecafe/",
+  //   "https://www.instagram.com/lafourmicreperie/",
+  //   "https://www.instagram.com/graobrasilcafe/",
+  //   "https://www.instagram.com/artesana.cafeteria/",
+  // ];
+  //#endregion
+
+  //Esconder div conforme opção selecionada
   if (value === "select") {
-  
     for (var i = 0; i < caixaCafe.length; i++) {
       caixaCafe[i].style.visibility = "hidden";
     }
-  }else if(value == 'campo-bom' || 'novo-hamburgo'){
+  } else if (value == "campo-bom" || "novo-hamburgo") {
     for (var i = 0; i < caixaCafe.length; i++) {
       caixaCafe[i].style.visibility = "visible";
     }
   }
 
   if (value === "campo-bom") {
-    for (var i = 0; i < cafe.length; i++) {
-      site[i].innerHTML = "Site";
-      instagram[i].innerHTML = "Instagram";
+    for (var i = 0; i < caixaCafe.length; i++) {
+      let { nome, siteUrl, insta } = cafeteriasCb[i];
+      console.log(site);
+      cafe[i].innerHTML = nome;
+      site[i].href = siteUrl;
+      instagram[i].href = insta;
     }
-    
-    cafe[0].innerHTML = nomescafe[0];
-    cafe[1].innerHTML = nomescafe[1];
-    cafe[2].innerHTML = nomescafe[2];
-
-    site[0].href = sitesLinks[0];
-    site[1].href = sitesLinks[1];
-    site[2].href = sitesLinks[2];
-
-    instagram[0].href = instaLink[0];
-    instagram[1].href = instaLink[1];
-    instagram[2].href = instaLink[2];
-
   } else if (value === "novo-hamburgo") {
-    cafe[0].innerHTML = nomescafe[3];
-    cafe[1].innerHTML = nomescafe[4];
-    cafe[2].innerHTML = nomescafe[5];
-
-    site[0].href = sitesLinks[3];
-    site[1].href = sitesLinks[4];
-    site[2].href = sitesLinks[5];
-
-    instagram[0].href = instaLink[3];
-    instagram[1].href = instaLink[4];
-    instagram[2].href = instaLink[5];
+    for (var i = 0; i < caixaCafe.length; i++) {
+      let { nome, siteUrl, insta } = cafeteriasNh[i];
+      console.log(site);
+      cafe[i].innerHTML = nome;
+      site[i].href = siteUrl;
+      instagram[i].href = insta;
+    }
   }
 }
-
-
 refreshSelect();
